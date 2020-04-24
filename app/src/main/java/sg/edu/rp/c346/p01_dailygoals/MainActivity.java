677187508c717673ffd76e2ea2cf6c5f91320c6f
ooishujing <18016012@myrp.edu.sg>
2020-04-24 15:46:28 +0800

    Basic completed

--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
@@ -2,7 +2,13 @@ package sg.edu.rp.c346.p01_dailygoals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1st qns
+                RadioGroup rg1 = (RadioGroup) findViewById(R.id.rg1);
+                int selectrb1 = rg1.getCheckedRadioButtonId();
+                RadioButton rb1 = (RadioButton) findViewById(selectrb1);
+                // 2nd qns
+                RadioGroup rg2 = (RadioGroup) findViewById(R.id.rg2);
+                int selectrb2 = rg2.getCheckedRadioButtonId();
+                RadioButton rb2 = (RadioButton) findViewById(selectrb2);
+                // 3rd qns
+                RadioGroup rg3 = (RadioGroup) findViewById(R.id.rg3);
+                int selectrb3 = rg3.getCheckedRadioButtonId();
+                RadioButton rb3 = (RadioButton) findViewById(selectrb3);
+                // 4th qns
+                EditText et = (EditText) findViewById(R.id.et) ;
+
+
+                String[] info = {rb1.getText().toString(),rb2.getText().toString(),rb3.getText().toString(),et.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this, Summary.class);
+                i.putExtra("info",info);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Summary.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Summary.java
@@ -2,7 +2,9 @@ package sg.edu.rp.c346.p01_dailygoals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        Intent i = getIntent();
+        String[] info = i.getStringArrayExtra("info");
+
+        TextView tv1 = findViewById(R.id.textView1);
+        TextView tv3 = findViewById(R.id.textView3);
+        TextView tv2 = findViewById(R.id.textView2);
+        TextView tv4 = findViewById(R.id.textView4);
+
+        tv1.setText("Read up on materials before class: "+info[0]);
+        tv2.setText("Arrive on time so as not to miss important part of the lesson: "+info[1]);
+        tv3.setText("Attempt the problem myself: "+info[2]);
+        tv4.setText("Reflection: "+info[3]);
     }
 }
